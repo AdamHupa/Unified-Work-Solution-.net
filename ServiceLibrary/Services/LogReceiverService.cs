08248c50da404eb6ac429bf940e4c59cb1c8db6f
ahupa-git <>
2018-11-11 23:02:48 +0100

    Set NLog service
    1. Set service configuration for library and host projects
    2. Add ClientAddress class.
    3. Implemented: NLog.LogReceiverService.ILogReceiverServer

--- ServiceLibrary/Services/LogReceiverService.cs
+++ ServiceLibrary/Services/LogReceiverService.cs
@@ -8,10 +8,17 @@ using ServiceLibrary.Contracts;
 using System.ServiceModel;
 using System.Xml.Linq;
 
+// link: https://github.com/jkowalski/NLog/blob/master/tests/NLogReceiverService/LogReceiverServer.cs
+
 namespace ServiceLibrary.Services
 {
-    public class LogReceiverService : ILogService, ILogServiceAsync
+    //[ServiceBehavior(Namespace = Namespaces.Logger)]
+    
+    public class LogReceiverService : NLog.LogReceiverService.ILogReceiverServer, ILogService, ILogServiceAsync
     {
+        private static NLog.Logger _logger = NLog.LogManager.GetCurrentClassLogger();
+
+
         public Task ReceiveLogA()
         {
             throw new NotImplementedException();
@@ -21,5 +28,28 @@ namespace ServiceLibrary.Services
         {
             throw new NotImplementedException();
         }
+
+
+        #region ILogReceiverServer Members
+
+        /// <remarks>Designed primarily for receiving critical events sent from crashing clients.</remarks>
+        public void ProcessLogMessages(NLog.LogReceiverService.NLogEvents events)
+        {
+            string address = Tools.ClientAddress.DeducedClientAddress();
+
+
+            IList<NLog.LogEventInfo> logEventInfos = events.ToEventInfo("Client.");
+
+            foreach (var logEventInfo in logEventInfos)
+            {
+                logEventInfo.Properties["Address"] = address;
+                //logEventInfo.Properties["Logger"] = logEventInfo.LoggerName;
+
+                NLog.Logger logger = NLog.LogManager.GetLogger(logEventInfo.LoggerName);
+                logger.Log(logEventInfo);
+            }
+        }
+
+        #endregion // ILogReceiverServer Members
     }
 }
---
+++ ServiceLibrary/Tools/ClientAddress.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+
+// link: https://stackoverflow.com/questions/33166679/get-client-ip-address-using-wcf-4-5-remoteendpointmessageproperty-in-load-balanc
+
+namespace ServiceLibrary.Tools
+{
+    internal class ClientAddress
+    {
+        public static string DeducedClientAddress()
+        {
+            string address = String.Empty;
+
+            try
+            {
+                OperationContext context = OperationContext.Current;
+                MessageProperties properties = context.IncomingMessageProperties;
+                RemoteEndpointMessageProperty endpoint = properties[RemoteEndpointMessageProperty.Name] as RemoteEndpointMessageProperty;
+
+                if (properties.Keys.Contains(HttpRequestMessageProperty.Name))
+                {
+                    HttpRequestMessageProperty endpointLoadBalancer = properties[HttpRequestMessageProperty.Name] as HttpRequestMessageProperty;
+                    if (endpointLoadBalancer != null && endpointLoadBalancer.Headers["X-Forwarded-For"] != null)
+                    {
+                        address = endpointLoadBalancer.Headers["X-Forwarded-For"];
+                    }
+                }
+                if (String.IsNullOrEmpty(address))
+                {
+                    address = endpoint.Address;
+                }
+            }
+            catch (Exception)
+            {
+                address = String.Empty;
+            }
+
+            return address;
+        }
+    }
+}
